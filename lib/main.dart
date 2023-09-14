e7123434372e175009888c58fac3f7adcdff8edb
Salumsu <hxhmichaelhxh@gmail.com>
2023-09-14 15:58:05 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[300],
+      appBar: AppBar(
+        title: const Text('I Am Poor'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Center(
+          child: Image(
+            image: AssetImage('images/homeless.png'),
+          ),
+        ),
+      ),
+    ),
+  ));
+}
