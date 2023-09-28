44cfa6ce23e3efe3564977ccec6b3d643f500f95
Salumsu <75105743+Salumsu@users.noreply.github.com>
2023-09-28 10:10:01 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       backgroundColor: Colors.blueGrey[300],
       appBar: AppBar(
